6b50f3ff4f14b7292fe9d153c61aa8f6cf45244a
Ilya Solovjov <ilya_solovjov@universumco.com>
2021-07-12 18:25:41 +0100

    Edited grid and creted random color and rotation.
    Co-authored-by: Cillian O'Neill <cillian_oneill@universumco.com>

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-document.body.onload = addElement;
+function randomColor () {
+    let colorValue = Math.floor(Math.random()*5);
+    color = `color${colorValue}`;
+}
+
+function randomRotation () {
+    let rotationValue = Math.floor(Math.random()*4);
+    rotation = `rotation${rotationValue}`;
+}
 
 function addElement () {
-  const newDiv = document.createElement("div");
+    const shapeContainer = document.querySelector('.shapes')
+    const newShape = document.createElement('div');
 
-  const newContent = document.createTextNode("Hi there and greetings!");
+    randomColor();
+    randomRotation();
 
-  newDiv.appendChild(newContent);
+    newShape.classList.add('base-shape', color, rotation);
+    shapeContainer.appendChild(newShape);
+}
 
-  const currentDiv = document.getElementById("shapes");
-  document.body.insertBefore(newDiv, currentDiv);
+for(let i = 0; i < 20; i++){
+    addElement();
 }
