060b09b28c7d3f1a0043dd799ec88c3c855c9fcd
Ilya Solovjov <ilya_solovjov@universumco.com>
2021-07-12 15:04:33 +0100

    Basic HTML and start of shape js.

---
+++ app.js
@@ -0,0 +1,12 @@
+document.body.onload = addElement;
+
+function addElement () {
+  const newDiv = document.createElement("div");
+
+  const newContent = document.createTextNode("Hi there and greetings!");
+
+  newDiv.appendChild(newContent);
+
+  const currentDiv = document.getElementById("shapes");
+  document.body.insertBefore(newDiv, currentDiv);
+}
