a27fbd55cab52f6604066a9fb0ceb3f510597dcb
Ilya Solovjov <ilya_solovjov@universumco.com>
2021-07-16 11:57:24 +0100

    Shapes before refactor.
    Co-authored-by: Cillian O'Neill <cillian_oneill@universumco.com>

--- app.js
+++ app.js
@@ -3,27 +3,27 @@ function randomRotation() {
   rotation = `rotation${rotationValue}`;
 }
 
-    const colorOrder1 = ["color3", "color2", "color0", "color1", "color4"];
-    const colorOrder2 = ["color1", "color4", "color3", "color2", "color0"];
-    const colorOrder3 = ["color2", "color0", "color1", "color4", "color3"];
-    const colorOrder4 = ["color4", "color3", "color2", "color0", "color1"];
-    const colorOrders = [colorOrder1, colorOrder2, colorOrder3, colorOrder4];
-    let colorOrderValue = colorOrders[Math.floor(Math.random()*4)];
-    let row = 1;
-    
-    function addShapes() {
-        for (colorOrder of colorOrders) {
-    const shapeContainer = document.querySelector(`.shapes-${row}`);
+const colorOrder1 = ["color3", "color2", "color0", "color1", "color4"];
+const colorOrder2 = ["color1", "color4", "color3", "color2", "color0"];
+const colorOrder3 = ["color2", "color0", "color1", "color4", "color3"];
+const colorOrder4 = ["color4", "color3", "color2", "color0", "color1"];
+const colorOrders = [colorOrder1, colorOrder2, colorOrder3, colorOrder4];
+// const colorOrdersSmall = [colorOrder1, colorOrder2, colorOrder3];
+let colorOrderValue = colorOrders[Math.floor(Math.random()*4)];
+let row = 1;
 
-    for (color of colorOrder) {
-      const newShape = document.createElement("div");
-      
-      randomRotation();
+function addShapes(rowNumber) {
 
-      newShape.classList.add("base-shape", color, rotation);
-      shapeContainer.appendChild(newShape);
+    for (colorOrder of colorOrders) {
+const shapeContainer = document.querySelector(`.shapes-${row}`);
+for (color of colorOrder) {
+  const newShape = document.createElement("div");
+  
+  randomRotation();
+  newShape.classList.add("base-shape", color, rotation);
+  shapeContainer.appendChild(newShape);
     }
-    row++;
+  row++;
   }
 }
 
